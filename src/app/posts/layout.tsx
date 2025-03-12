546e3bd54c0c87b4fcdb370e8c4ff37e83041718
Dushime Brother <brother.dushime@gmail.com>
2025-03-12 14:40:53 +0200

    updated the post layout

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -1,16 +1,19 @@
 import ClientPage from "@/components/layout/ClientPage";
+import { getSessionUser } from "@/server-actions/user.actions";
+import { isDateLaterThanToday } from "@/util/DateFunctions";
+import { redirect } from "next/navigation";
 
 export default async function PostsPageLayout({children}:{children: React.ReactNode}) {
-     // const {user} = await getSessionUser();
-     // if(!user) {
-     //      return redirect('/auth/login');
-     // }
-     // if(user.admin) return redirect('/dashboard/admin');
-     // if(user.vendor) return redirect('/dashboard/vendor');
+     const {user} = await getSessionUser();
+     if(!user) {
+          return redirect('/auth/login');
+     }
+     if(user.admin) return redirect('/dashboard/admin');
+     if(user.vendor) return redirect('/dashboard/vendor');
 
-     // if(user && (!user.client?.subscription || !isDateLaterThanToday(user.client.subscription.expiryAt))) {
-     //      return redirect('/subscribe');
-     // }
+     if(user && (!user.client?.subscription || !isDateLaterThanToday(user.client.subscription.expiryAt))) {
+          return redirect('/subscribe');
+     }
      return (
           <ClientPage>
                {children}
