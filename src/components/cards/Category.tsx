6b6fcbfc30fbda05c43e7e3cedb41e4657072676
Dushime Brother <brother.dushime@gmail.com>
2025-03-16 08:18:11 +0200

    feat: updates category card

--- src/components/cards/Category.tsx
+++ src/components/cards/Category.tsx
@@ -6,22 +6,25 @@ import { formatPrice } from "@/util/stringFuncs";
 import Link from "next/link";
 import defaultImage from '../../../public/images/default-image.jpg'
 import Image from "next/image";
+import { FaArrowRight } from "react-icons/fa6";
 
 export const CategoryCard = ({category}:{category: TCategory}) => {
      const {user, setAuth}  = useAuthContext();
      const viewText = `View ${formatPrice(category._count.posts)} vendors`;
      return (
           <div className="w-full flex flex-col items-center justify-between gap-[10px] rounded-[5px] shadow-sm shadow-gray-300 overflow-hidden">
-               <Image src={category.icon ? category.icon : defaultImage} alt={category.name} width={800} height={450} className="w-full aspect-video object-fill" placeholder="blur"   />
-               <div className="w-full flex flex-col items-center justify-start px-[20px] gap-[5px]">
-                    <h3 className="text-[0.9rem] font-bold text-black">{category.name}</h3>
+               <Image src={category.icon ? category.icon : defaultImage} alt={category.name} width={800} height={450} className="w-full aspect-video object-fill" placeholder="empty"   />
+               <div className="w-full flex flex-col items-center justify-start px-[10px] gap-[5px]">
+                    <h3 className="text-[1.1rem] font-bold text-black">{category.name}</h3>
                     <p className="text-[0.8rem] text-gray-600 text-center line-clamp-3">{category.description}</p>
                </div>
-               {
-                    !user ? 
-                    <button type="button" onClick={setAuth}>{viewText}</button>:
-                    <Link href={`/posts?category=${category.id}`} prefetch={true}>{viewText}</Link>
-               }
+               <div className="w-full py-[5px] flex items-center justify-center">
+                    {
+                         !user ? 
+                         <button className="text-center w-auto py-[7.5px] bg-white border-[1.3px] font-semibold border-blue-600 text-blue flex items-center gap-[5px] rounded-[30px] text-[1rem] text-blue-800 px-[20px]" type="button" onClick={setAuth}>{viewText} <i className="text-[22px] font-normal"><FaArrowRight /></i></button>:
+                         <Link className="text-center w-auto py-[7.5px] bg-white border-[1.3px] font-semibold border-blue-600 text-blue flex items-center gap-[5px] rounded-[30px] text-[1rem] text-blue-800 px-[20px]" href={`/posts?category=${category.id}`} prefetch={true}>{viewText} <i className="text-[22px]"><FaArrowRight /></i></Link>
+                    }
+               </div>
           </div>
      )
 }
