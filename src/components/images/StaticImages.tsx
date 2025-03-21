6e53c1f95bc02e453697d7209acc3bbdf2b8e387
Dushime Brother <brother.dushime@gmail.com>
2025-03-21 12:34:03 +0200

    updates trending section

--- src/components/images/StaticImages.tsx
+++ src/components/images/StaticImages.tsx
@@ -4,10 +4,10 @@ import Image2 from '../../../public/trending/image2.jpg';
 import Image3 from '../../../public/trending/image3.jpg';
 import Image4 from '../../../public/trending/image4.jpg';
 import Image5 from '../../../public/trending/image5.jpg';
-import Image6 from '../../../public/trending/image6.jpg';
+// import Image6 from '../../../public/trending/image6.jpg';
 import Image7 from '../../../public/trending/image7.jpg';
 import Image8 from '../../../public/trending/image8.jpg';
 
 export const TrendingImages:StaticImport[]  = [
-     Image1,Image2, Image3, Image4, Image5, Image6, Image7, Image8
+     Image1,Image2, Image3, Image4, Image5, Image7, Image8
 ]
