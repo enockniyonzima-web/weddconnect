4018b4f4dde19739f0195edbf9b4f223482b3edb
Dushime Brother <brother.dushime@gmail.com>
2025-03-24 17:04:24 +0200

    feat: update feature card

--- src/components/cards/PostFeatureCard.tsx
+++ src/components/cards/PostFeatureCard.tsx
@@ -6,9 +6,9 @@ export const PostFeatureCard = ({feature}: {feature:TPostFeature}) => {
      return(
           <div title={feature.categoryFeature.name} className="w-auto cursor-pointer flex flex-col justify-start items-center gap-[3px] rounded-[2.5px] p-[2.5px] border border-gray-200 bg-white ">
                <p className='text-[0.65rem] text-black font-bold'>{feature.categoryFeature.name}</p>
-               <div className="w-auto flex items-center gap-[2px] ">
+               <div className="w-auto max-w-[100%] flex items-center gap-[2px] overflow-hidden">
                     {Icon && <i className="text-gray-400 text-[18px] md:text-[18px] p-[1px]"><Icon /></i>}
-                    <span className="text-[0.85rem] lg:text-[0.7rem] text-black font-semibold whitespace-nowrap">: {feature.value}</span>
+                    <span className="text-[0.85rem] lg:text-[0.7rem] text-black font-semibold whitespace-nowrap w-[70%] line-clamp-1">: {feature.categoryFeature.name.toLowerCase() === "languages"? feature.value.trim().split(",").length : feature.value}</span>
                </div>
           </div>
      )
@@ -16,13 +16,13 @@ export const PostFeatureCard = ({feature}: {feature:TPostFeature}) => {
 
 export const LGPostFeatureCard = ({feature}: {feature:TPostFeature}) => {
      const Icon = findIconByKey(feature.categoryFeature.icon);
-        return(
-             <div className="w-full flex justify-start items-center gap-[3px] p-[5px] px-[10px] border-b border-gray-800 ">
-                  <div className="w-auto flex items-center gap-[5px] ">
-                       {Icon && <i className="text-gray-400 text-[26px] md:text-[22px] p-[2px]"><Icon /></i>}
-                       <span className="text-[0.9rem] leading-3 lg:text-[0.8rem] text-gray-400 whitespace-nowrap">{feature.categoryFeature.name}: </span>
-                  </div>
-                  <span className="text-[0.9rem] lg:text-[0.85rem] text-gray-200 font-semibold whitespace-nowrap">{feature.value}</span>
-             </div>
-        )
-   }
+     return(
+          <div className="w-full flex justify-start items-center gap-[3px] p-[5px] px-[10px] border-b border-gray-800 ">
+               <div className="w-auto flex items-center gap-[5px] ">
+                    {Icon && <i className="text-gray-400 text-[26px] md:text-[22px] p-[2px]"><Icon /></i>}
+                    <span className="text-[0.9rem] leading-3 lg:text-[0.8rem] text-gray-400 whitespace-nowrap">{feature.categoryFeature.name}: </span>
+               </div>
+               <span className="text-[0.9rem] lg:text-[0.85rem] text-gray-200 font-semibold whitespace-nowrap">{feature.value}</span>
+          </div>
+     )
+}
